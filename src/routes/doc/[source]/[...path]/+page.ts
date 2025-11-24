3b5025497fa2fa359b27cdd3d6b2e36734ecc73f
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-24 00:21:17 -0800

    Refactor load function to use params instead of route

--- src/routes/doc/[source]/[...path]/+page.ts
+++ src/routes/doc/[source]/[...path]/+page.ts
@@ -1,9 +1,5 @@
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = ({ route }) => {
-	console.log(route.id); // '/a/[b]/[...c]'
-};
-
 export const load: PageLoad = ({ params }) => {
   const { source, path } = params;
 
